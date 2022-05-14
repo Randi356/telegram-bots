48e7ab9b50080912fb603014c07ea5c078d59d80
Cute Inspire <Randywahidin321@gmail.com>
2022-05-14 17:22:24 +0700

    added ultroid

---
+++ ultroid.py
@@ -0,0 +1,22 @@
+#!/bin/python
+import os 
+
+#Credits code by Rendy
+#telegram @FFmpegNotInstalled
+os.system("clear")
+os.system("sleep 2")
+
+print ("\033[1;32mAUTHOR :  \033[1;34mDeleveoper By Rendy")
+print ("\033[1;33mTelegram : @FFmpegNotInstalled")
+print ("\033[1;34mINSTALL ULTROID USERBOT")
+print
+print ("INSTALL ULTROID LOCAL..........")
+os.system("sleep 10")
+os.system("sudo pip3 install -U pip")
+os.system("sudo apt-get install python3-pip ffmpeg -y")
+os.system("sudo apt-get install screen -y")
+os.system("git clone https://github.com/TeamUltroid/Ultroid && cd Ultroid")
+os.system("clear")
+os.system("sleep 5")
+os.system("pip3 install -r requirements.txt")
+os.system("pip3 install --no-cache-dir re*/st*/op*.txt")
